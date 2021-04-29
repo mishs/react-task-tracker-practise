7e5b1caeea4793fc4a74f89b6775c9f2981e43ae
Mish <mishuct26@gmail.com>
2021-04-29 10:31:03 +0200

    On state updating, the right away vs wrong way

--- react-task-tracker-tm-ap28-12/src/components/Tasks.js
+++ react-task-tracker-tm-ap28-12/src/components/Tasks.js
@@ -24,6 +24,14 @@ const Tasks = () => {
             date: '28 Feb 2020'
         }     
     ])
+    // NOTE-
+    // setTask is when you want to change or update state;
+    // but state is immutable, so you can't/shouldn't do tasks.push
+    // state is not some thing you can directly change. (cause issues). Rather you Recreate it and send it down- 
+    // in our case using this setTask. 
+    // Reminder,  ...prevTask, {updates state in here}
+    //   ?as in counter: counter + 1?
+
     return (
         // ALWAYS REMEMBER, HERE RETURNS OBJECT, SO CURLY BRACKETS INSIDE
         <div>
