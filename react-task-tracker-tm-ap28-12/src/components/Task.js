0b7b0722c7a500f7045369be2a0139479e4e397a
Mish <mishuct26@gmail.com>
2021-04-29 11:32:36 +0200

    Add fa specified font-icon library

--- react-task-tracker-tm-ap28-12/src/components/Task.js
+++ react-task-tracker-tm-ap28-12/src/components/Task.js
@@ -1,7 +1,8 @@
+import { FaTimes } from 'react-icons/fa'
 const Task = ({ taskProp }) => {
     return (
         <div className='task'>
-        <h3>{taskProp.chore}</h3>
+        <h3>{taskProp.chore} <FaTimes style={{ color: 'red', cursor: 'pointer'}} /></h3>
         <p>{taskProp.date}</p>
         </div>
     )
