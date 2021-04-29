94cce19f40ece8f07733e14ff452e0d28c4d22a1
Mish <mishuct26@gmail.com>
2021-04-29 10:25:16 +0200

    Accessing data and displaying it from state using setState

--- react-task-tracker-tm-ap28-12/src/components/Tasks.js
+++ react-task-tracker-tm-ap28-12/src/components/Tasks.js
@@ -1,29 +1,29 @@
-const tasksListed = [
-    {
-        id: 1,
-        chore: 'Doors Appointment',
-        date: '28 Feb 2020',
-        status: 'Done'
-    },
-    {
-        id: 2,
-        chore: 'Second Appointment',
-        date: '28 Feb 2020'
-    }, 
-    {
-        id: 3,
-        chore: 'Third Appointment',
-        date: '28 Feb 2020'
-    },   
-    {
-        id: 4,
-        chore: 'Fourth Appointment',
-        date: '28 Feb 2020'
-    }     
-]
-
+import { useState } from 'react'
+ 
 const Tasks = () => {
-
+    const [tasksListed, setTask ] = useState([
+        {
+            id: 1,
+            chore: 'Doors Appointment Date',
+            date: '28 Feb 2020',
+            status: 'Done'
+        },
+        {
+            id: 2,
+            chore: 'Second Appointment Up',
+            date: '28 Feb 2020'
+        }, 
+        {
+            id: 3,
+            chore: 'Third Appointment',
+            date: '28 Feb 2020'
+        },   
+        {
+            id: 4,
+            chore: 'Fourth Appointment',
+            date: '28 Feb 2020'
+        }     
+    ])
     return (
         // ALWAYS REMEMBER, HERE RETURNS OBJECT, SO CURLY BRACKETS INSIDE
         <div>
