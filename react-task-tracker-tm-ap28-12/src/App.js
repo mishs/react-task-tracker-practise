561528273375b1d0369565f82667830817e2f800
Mish <mishuct26@gmail.com>
2021-04-29 10:49:16 +0200

    Lifting state to global/app state =ContextAPI/Redu

--- react-task-tracker-tm-ap28-12/src/App.js
+++ react-task-tracker-tm-ap28-12/src/App.js
@@ -1,7 +1,31 @@
+import { useState } from 'react'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasksArrHereIsAlsoState, setTask ] = useState([
+    {
+        id: 1,
+        chore: 'Doors Appointment Date',
+        date: '28 Feb 2020',
+        status: 'Done'
+    },
+    {
+        id: 2,
+        chore: 'Second Appointment Up',
+        date: '28 Feb 2020'
+    }, 
+    {
+        id: 3,
+        chore: 'Third Appointment',
+        date: '28 Feb 2020'
+    },   
+    {
+        id: 4,
+        chore: 'Fourth Appointment',
+        date: '28 Feb 2020'
+    }     
+])
   
   return (
     <div className="App">
